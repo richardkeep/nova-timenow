321354fd2c580a8ba66c0ca261a62a055abaef91
Richard Keep <r.kipsang@gmail.com>
2018-09-16 15:25:24 +0000

    Few tweaks

--- resources/js/components/Card.vue
+++ resources/js/components/Card.vue
@@ -1,20 +1,17 @@
 <template>
     <card class="flex flex-col items-center justify-center">
+        <div class="flex mb-4">
+            <h3 class="mr-3 text-base text-80 font-bold">Time Now</h3>
+            <select v-model="currentTimezone" class="ml-auto min-w-24 h-6 text-xs no-appearance bg-40">
+                <option v-for="timezone in card.timezones" :value="timezone">{{ timezone }}</option>
+            </select>
+        </div>
 
-                <div class="flex mb-4">
-                    <h3 class="mr-3 text-base text-80 font-bold">Time</h3>
-                    <select v-model="currentTimezone" class="ml-auto min-w-24 h-6 text-xs no-appearance bg-40">
-                        <option v-for="timezone in timezones" :value="timezone">{{ timezone }}</option>
-                    </select>
-                </div>
+        <p class="flex items-center text-4xl mb-4">{{ time }}</p>
 
-                <p class="flex items-center text-4xl mb-4">{{ time }}</p>
-
-                <div class="flex items-center">
-                    <p class="text-80 font-bold">{{ date }}</p>
-                </div>
-
-
+        <div class="flex items-center">
+            <p class="text-80 font-bold">{{ date }}</p>
+        </div>
     </card>
 </template>
 
