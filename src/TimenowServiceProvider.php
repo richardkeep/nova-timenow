54ea32113f407b2dd136860b0e62f7dc0bce7afe
richardkeep <r.kipsang@gmail.com>
2019-12-18 09:27:36 +0300

    fix styles

--- src/TimenowServiceProvider.php
+++ src/TimenowServiceProvider.php
@@ -20,8 +20,8 @@ class TimenowServiceProvider extends ServiceProvider
         });
 
         Nova::serving(function () {
-            Nova::script('nova-timenow', __DIR__ . '/../dist/js/card.js');
-            Nova::style('nova-timenow', __DIR__ . '/../dist/css/card.css');
+            Nova::script('nova-timenow', __DIR__ .'/../dist/js/card.js');
+            Nova::style('nova-timenow', __DIR__ .'/../dist/css/card.css');
         });
     }
 
@@ -48,6 +48,6 @@ class TimenowServiceProvider extends ServiceProvider
 
         Route::middleware(['nova'])
                 ->prefix('nova-vendor/nova-timenow')
-                ->group(__DIR__ . '/../routes/api.php');
+                ->group(__DIR__ .'/../routes/api.php');
     }
 }
